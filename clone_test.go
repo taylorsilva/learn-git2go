b2b525df18d77f273d73caf8841f186b87a7a322
Taylor Silva <taylorsilva@outlook.com>
2019-11-10 18:07:06 -0500

    Basic how to clone tests
    Signed-off-by: Taylor Silva <taylorsilva@outlook.com>

--- clone_test.go
+++ clone_test.go
@@ -25,9 +25,33 @@ var _ = Describe("Cloning a git repo", func() {
 		Expect(err).ToNot(HaveOccurred())
 	})
 
-	Context("Cloning a http url", func() {
+	Context("Cloning a repo", func() {
 		It("with https url", func() {
 			_, err = git.Clone("https://github.com/libgit2/git2go.git", tmpRepoDir, &git.CloneOptions{})
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("with ssh", func() {
+			publicKeyPath := ""
+			privateKeyPath := ""
+			passphrase := ""
+			options := &git.CloneOptions{
+				FetchOptions: &git.FetchOptions{
+					RemoteCallbacks: git.RemoteCallbacks{
+						CredentialsCallback: func(url string, username_from_url string, allowed_types git.CredType) (git.ErrorCode, *git.Cred) {
+							i, c := git.NewCredSshKey(username_from_url, publicKeyPath, privateKeyPath, passphrase)
+							return git.MakeGitError2(i).(*git.GitError).Code, &c
+						},
+						CertificateCheckCallback: func(cert *git.Certificate, valid bool, hostname string) git.ErrorCode {
+							if valid {
+								return git.ErrOk
+							}
+							return git.ErrOk
+						},
+					},
+				},
+			}
+			_, err = git.Clone("git@github.com:libgit2/git2go.git", tmpRepoDir, options)
 			Expect(err).ToNot(HaveOccurred())
 		})
 
